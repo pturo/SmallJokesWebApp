8bdb14d9a106b321b3a06b7889fb8d51617365aa
Paweł Turoń <34440020+pturo@users.noreply.github.com>
2020-11-17 19:29:18 +0100

    Add project to repo
    Add whole project to repo.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SmallJokesWebApp.Models;
+
+namespace SmallJokesWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<SmallJokesWebApp.Models.Joke> Joke { get; set; }
+    }
+}
